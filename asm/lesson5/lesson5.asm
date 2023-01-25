757d132aeb15b06d0af0411cce33c6a2ab06f290
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-01-24 17:19:39 -0800

    why push and pop certain registers?

--- asm/lesson5/lesson5.asm
+++ asm/lesson5/lesson5.asm
@@ -1,5 +1,29 @@
 ;external include files
 
+;when do you choose to push registers onto the stack?
+	; for the functions file it was prbably because this program might have had used those registers 
+		; for something else entirely
+
+%include 'functions.asm'
+
+SECTION .data
+msg1 db 'Hello, brave new world!' , 0Ah
+msg2 db 'This is how we recycle in NASM.', 0Ah
+
+SECTION .text
+global _start
+
+_start:
+
+	mov eax, msg1
+	call sprint
+
+	mov eax, msg2
+	call sprint
+
+
+	call quit
+
 
 
 
