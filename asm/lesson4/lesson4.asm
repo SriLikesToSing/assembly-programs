af72f10482a66381261409e8fa9d621f7678c6ac
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-01-24 17:01:22 -0800

    more elegant

--- asm/lesson4/lesson4.asm
+++ asm/lesson4/lesson4.asm
@@ -1,15 +1,13 @@
 ;subroutines
 ; what are subroutines?
-	/*'
-	  - they are better than jmp instruction because it automatically returns to address after executing
-	  	- behaves like a function
+	
+;	  - they are better than jmp instruction because it automatically returns to address after executing
+;	  	- behaves like a function
 
-
-	  - what is the difference between jmp and call?
-	  	- jmp just jumps directly to the memory address you tell it to 
-		- call stores the return value of the current address onto the stack to come back to it
-			after the call is executed
-	*/
+;	  - what is the difference between jmp and call?
+;	  	- jmp just jumps directly to the memory address you tell it to 
+;		- call stores the return value of the current address onto the stack to come back to it
+;			after the call is executed
 
 
 SECTION .data
@@ -26,11 +24,11 @@ _start:
 	mov edx, eax
 	mov ecx, msg
 	mov ebx, 1
-	move eax, 4
+	mov eax, 4
 	int 80h
 
 	; returns before continuing again to strlen: 
-	move ebx, 0
+	mov ebx, 0
 	mov eax, 1
 	int 80h
 
